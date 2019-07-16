0a4c57393852fa1885406239ba63573584ff4db2
Drew Warren <drewwarrentiy@gmail.com>
2019-07-15 20:18:18 -0600

    Second commit

--- scripts.js
+++ scripts.js
@@ -1,1 +1,10 @@
-console.log("No scripts yet :(");
+"use strict";
+
+function firstBuild() {
+  const app = document.getElementById("app");
+  const helloHeader = document.createElement("h1");
+  app.appendChild(helloHeader);
+  helloHeader.innerHTML = "Hello World!";
+}
+
+firstBuild();
